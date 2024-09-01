e1f4cd2463623e124eeb26070ac71323a558e057
William Garcia <59988459+Meta-Willi@users.noreply.github.com>
2024-08-31 23:37:45 -0600

    Comentarios index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
+// contenedor donde se mostrarán las tarjetas de productos
 const cardContainer = document.getElementById("producto-container");
- 
+// Función para crear las tarjetas
 function crearProductos(products) {
-   const memoria = JSON.parse(localStorage.getItem("products")) || [];
+   const memoria = JSON.parse(localStorage.getItem("products")) || [];  // Recupera los productos almacenados en el carrito desde localStorage o inicia con un array vacío
      
 
    products.forEach(producto => {
+     // se verifica si el producto ya está en el carrito
       const productoEnCarrito = memoria.find(product => product.id === producto.id);
       if (productoEnCarrito) {
+          // se ajusta la cantidad disponible en el inventario, si el producto ya está
            producto.cantidadDisponible -= productoEnCarrito.cantidad;
        }
-
+      // Define el contenido HTML  
       const nuevoProducto = document.createElement("div");
       nuevoProducto.classList = "producto-card";
       nuevoProducto.innerHTML = ` 
@@ -29,18 +32,21 @@ function crearProductos(products) {
                </div>
             </div>
        `
+     // se añade la nueva tarjeta de producto al contenedor en el DOM
       cardContainer.appendChild(nuevoProducto);
       
       nuevoProducto.getElementsByTagName("button")[0].addEventListener("click", () => {
          const cantidadInput = nuevoProducto.querySelector('.cantidad-input');
          const cantidadDeseada = parseInt(cantidadInput.value);
-         
+          // Verificar si la cantidad deseada es válida
          if (cantidadDeseada > 0 ) {
             if(cantidadDeseada <= producto.cantidadDisponible){
+              // Si la cantidad deseada es menor o igual a la disponible, agrega el producto al carrito
                agregarProducto(producto, cantidadDeseada);
                producto.cantidadDisponible -= cantidadDeseada;  
                nuevoProducto.querySelector('.producto-info p:last-of-type').innerText = `Disponibles: ${producto.cantidadDisponible}`;
             }else{
+             // validaciones para agregar productos
                if(producto.cantidadDisponible == 0){
                   alert('Ya no hay más de este producto, intenta agregar uno distinto' );    
                }else{
